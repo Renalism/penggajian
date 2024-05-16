fe7bffed2e21a0d4aaab1797241c54412c0fa33a
Renalism <denbagus.renal@gmail.com>
2024-05-16 21:58:24 +0800

    first commit

---
+++ pages/bagian.php
@@ -0,0 +1,51 @@
+
+            <div class="container-fluid px-4">
+                <div class="row">
+                    <div class="col">
+                        <h3>Bagian</h3>
+                    </div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col">
+                        <table class="table bg-white rounded shadow-sm  table-hover">
+                            <thead>
+                                <tr>
+                                    <th scope="col" width="50">#</th>
+                                    <th scope="col">Bagian</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr class="align-middle">
+                                    <th scope="row">1</th>
+                                    <td>HRD</td>
+                                </tr>
+                                <tr class="align-middle">
+                                    <th scope="row">2</th>
+                                    <td>Finance</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!-- /#page-content-wrapper -->
+    </div>
+
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
+        crossorigin="anonymous"></script>
+    <script>
+        var el = document.getElementById("wrapper");
+        var toggleButton = document.getElementById("menu-toggle");
+
+        toggleButton.onclick = function () {
+            el.classList.toggle("toggled");
+        };
+    </script>
+</body>
+
+</html>
