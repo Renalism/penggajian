9cd66d18fedb4220988c0cdbfe733f0c6c899821
Renalism <denbagus.renal@gmail.com>
2024-06-10 16:34:26 +0800

    changes

---
+++ pages/bagiantambah.php
@@ -0,0 +1,17 @@
+<div id="top" class="row mb-3 col">
+    <div class="col">
+        <h3>Tambah Data Bagian</h3>
+    </div>
+    <div class="col">
+        <a href="?pages=bagian" class="btn btn-primary floath-end">
+            <i class="fa fa-arrow-circle-left"></i>
+            Kembali
+        </a>
+    </div>
+</div>
+<div id="pesan"class="row mb-3 col">
+    <div class="col"></div>
+</div>
+<div id="inputan" class="row mb-3 col">
+    <div class="col"></div>
+</div>
