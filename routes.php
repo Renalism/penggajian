e1e858af4ea2bcbcf781e16f53f33e78f1fb15ea
Renalism <denbagus.renal@gmail.com>
2024-06-13 16:52:58 +0800

    changes

--- routes.php
+++ routes.php
@@ -33,6 +33,9 @@ case "karyawantambah";
 case "karyawanhapus";
         include "pages/karyawan/karyawanhapus.php";
         break;
+case "karyawanubah";
+        include "pages/karyawan/karyawanubah.php";
+        break;
 default:
         include "pages/404.php";
 
